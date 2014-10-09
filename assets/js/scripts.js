4d0230568a59cee08d93f14c10f1d5df67fa1d68
Nils Mattisson <nils.mattisson@gmail.com>
2014-10-09 19:00:48 +0800

    Updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -33,3 +33,15 @@ $('#mc-form').ajaxChimp({
 $('#mc-form-launch').ajaxChimp({
     url: 'http://formdevices.us8.list-manage.com/subscribe/post?u=148eabee2d3bdf2c67c658db9&amp;id=089d16bcd3'
 });
+
+// Twitter Follow button
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+// Facebook Like button
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
