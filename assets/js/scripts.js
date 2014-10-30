e42f596d0e42aeb9c91db1d0e7f1a100c6f0ab88
Marcus Ljungblad <marcus@ljungblad.nu>
2014-10-30 14:41:58 -0700

    Updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -51,7 +51,7 @@ $("#press-sidebar").affix();
 
 // countdown clock to kickstarter launch
 var d1 = new Date();
-var d2 = new Date('2014-11-04 00:00:00');
+var d2 = new Date(2014, 10, 4, 0, 0, 0);
 var seconds =  (d2 - d1)/1000;
 var clock = $('.clock').FlipClock(seconds, {
   clockFace: 'DailyCounter',
