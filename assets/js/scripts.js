0d76143995f424e4380ede3a3abf09facc8effd8
Marcus Ljungblad <marcus@ljungblad.nu>
2014-12-04 14:48:46 +0100

    Updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -50,12 +50,58 @@ $('#mc-form-launch-bottom').ajaxChimp({
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-// countdown clock to kickstarter launch
-var d1 = new Date();
-var d2 = new Date(2014, 10, 4, 0, 0, 0);
-var seconds =  (d2 - d1)/1000;
-var clock = $('.clock').FlipClock(seconds, {
-  clockFace: 'DailyCounter',
-  countdown: true,
-  showSeconds: true
-});
+// ip lookup
+var toggleCeleryStore = function() {
+  if ($('#celery-checkout-embed').attr('data-celery') === '547f1dde99d46e0500e58373') {
+    // use US+Intl store
+    $('.celery-checkout').html('<div id="celery-checkout-embed" data-celery="54721d7825177305002d92d7" data-celery-type="embed" data-celery-version="v2">Checkout US/International</div>');
+    $('#celery-checkout-toggle').html('US & International | <a href="#" onclick="toggleCeleryStore()">EU</a>');
+  } else {
+    // use EU store
+    $('#celery-checkout-toggle').html('<a href="#" onclick="toggleCeleryStore()">US & International</a> | EU');
+    $('.celery-checkout').html('<div id="celery-checkout-embed" data-celery="547f1dde99d46e0500e58373" data-celery-type="embed" data-celery-version="v2">Checkout EU</div>');
+  }
+  Celery.initialize();
+}
+
+var celerylocationcallback = function(data) {
+  var memberstates = [
+        "BE",
+        "BG",
+        "CZ",
+        "DK",
+        "DE",
+        "EE",
+        "IE",
+        "EL",
+        "ES",
+        "FR",
+        "IT",
+        "CY",
+        "LV",
+        "LT",
+        "LU",
+        "HU",
+        "MT",
+        "NL",
+        "AT",
+        "PL",
+        "PT",
+        "RO",
+        "SI",
+        "SK",
+        "FI",
+        "SE",
+        "UK"
+    ];
+  if (data.statusCode === "OK" && data.countryCode) {
+    if ($.inArray(data.countryCode, memberstates) > -1) {
+      // visitor is from EU
+      $('#celery-checkout-toggle').html('<a href="#" onclick="toggleCeleryStore()">US & International</a> | EU');
+      $('.celery-checkout').html('<div id="celery-checkout-embed" data-celery="547f1dde99d46e0500e58373" data-celery-type="embed" data-celery-version="v2">Checkout</div>');
+    }
+  }
+  Celery.initialize();
+};
+
+$.getScript("http://api.ipinfodb.com/v3/ip-country/?key=8570f1b66c74cfa95f093d09a8c418f76a4066c93c5559c600d92cf296352767&format=json&callback=celerylocationcallback");
